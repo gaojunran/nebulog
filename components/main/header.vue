42155f6a07f18dc74551c226ac472ec6b0478f60
gaojunran <nebula2021@126.com>
2024-05-12 22:13:09 +0800

    initial

---
+++ components/main/header.vue
@@ -0,0 +1,69 @@
+<script setup lang="ts">
+const colorMode = useColorMode()
+function onClick(val: string) {
+  colorMode.preference = val
+}
+</script>
+
+<template>
+  <div class="py-5 border-b dark:border-gray-800  font-semibold">
+    <div class="flex px-6 container max-w-5xl justify-between mx-auto items-baseline ">
+      <ul class="flex items-baseline space-x-5">
+        <li class="text-base sm:text-2xl font-bold">
+          <NuxtLink to="/">
+            Riyad's Blog
+          </NuxtLink>
+        </li>
+      </ul>
+      <ul class="flex items-center space-x-3 sm:space-x-6 text-sm sm:text-lg">
+        <li>
+          <NuxtLink to="/blogs">
+            Blogs
+          </NuxtLink>
+        </li>
+        <li>
+          <NuxtLink to="/categories">
+            Categories
+          </NuxtLink>
+        </li>
+        <li title="About Me">
+          <NuxtLink to="/about" aria-label="About me">
+            About
+          </NuxtLink>
+        </li>
+        <li>
+          <ClientOnly>
+            <button
+              v-if="colorMode.value === 'light'"
+              name="light-mode"
+              title="Light"
+              class="hover:scale-110 transition-all ease-out hover:cursor-pointer"
+              @click="onClick('dark')"
+            >
+              <Icon name="icon-park:moon" size="20" />
+            </button>
+            <button
+              v-if="colorMode.value === 'dark'"
+              name="dark-mode"
+              title="Dark"
+              class="hover:scale-110 transition-all ease-out hover:cursor-pointer"
+              @click="onClick('light')"
+            >
+              <Icon name="noto:sun" size="20" />
+            </button>
+            <template #fallback>
+              <!-- this will be rendered on server side -->
+              <Icon name="svg-spinners:180-ring" size="20" />
+            </template>
+          </ClientOnly>
+        </li>
+      </ul>
+    </div>
+  </div>
+</template>
+
+<style>
+.router-link-active .router-link-exact-active  {
+  @apply underline
+}
+</style>
---
+++ pages/blogs/[blog].vue
@@ -0,0 +1,124 @@
+<script setup lang="ts">
+import type { BlogPost } from '@/types/blog'
+
+const { path } = useRoute()
+
+const { data: articles, error } = await useAsyncData(`blog-post-${path}`, () => queryContent(path).findOne())
+
+if (error.value)
+  navigateTo('/404')
+
+const data = computed<BlogPost>(() => {
+  return {
+    title: articles.value?.title || 'no-title available',
+    description: articles.value?.description || 'no-description available',
+    image: articles.value?.image || '/not-found.jpg',
+    alt: articles.value?.alt || 'no alter data available',
+    ogImage: articles.value?.ogImage || '/not-found.jpg',
+    date: articles.value?.date || 'not-date-available',
+    tags: articles.value?.tags || [],
+    published: articles.value?.published || false,
+  }
+})
+
+useHead({
+  title: data.value.title || '',
+  meta: [
+    { name: 'description', content: data.value.description },
+    {
+      name: 'description',
+      content: data.value.description,
+    },
+    // Test on: https://developers.facebook.com/tools/debug/ or https://socialsharepreview.com/
+    { property: 'og:site_name', content: 'Riyad\'s Blog' },
+    { hid: 'og:type', property: 'og:type', content: 'website' },
+    {
+      property: 'og:url',
+      content: `https://blog-nurriyad.vercel.app/${path}`,
+    },
+    {
+      property: 'og:title',
+      content: data.value.title,
+    },
+    {
+      property: 'og:description',
+      content: data.value.description,
+    },
+    {
+      property: 'og:image',
+      content: data.value.ogImage || data.value.image,
+    },
+    // Test on: https://cards-dev.twitter.com/validator or https://socialsharepreview.com/
+    { name: 'twitter:site', content: '@qdnvubp' },
+    { name: 'twitter:card', content: 'summary_large_image' },
+    {
+      name: 'twitter:url',
+      content: `https://blog-nurriyad.vercel.app/${path}`,
+    },
+    {
+      name: 'twitter:title',
+      content: data.value.title,
+    },
+    {
+      name: 'twitter:description',
+      content: data.value.description,
+    },
+    {
+      name: 'twitter:image',
+      content: data.value.ogImage || data.value.image,
+    },
+  ],
+  link: [
+    {
+      rel: 'canonical',
+      href: `https://blog-nurriyad.vercel.app/${path}`,
+    },
+  ],
+})
+
+// Generate OG Image
+defineOgImageComponent('Test', {
+  headline: 'Greetings 👋',
+  title: data.value.title || '',
+  description: data.value.description || '',
+  link: data.value.ogImage,
+
+})
+</script>
+
+<template>
+  <div class="px-6 container max-w-5xl mx-auto sm:grid grid-cols-12 gap-x-12 ">
+    <div class="col-span-12 lg:col-span-9">
+      <BlogHeader
+        :title="data.title"
+        :image="data.image"
+        :alt="data.alt"
+        :date="data.date"
+        :description="data.description"
+        :tags="data.tags"
+      />
+      <div
+        class="prose prose-pre:max-w-xs sm:prose-pre:max-w-full prose-sm sm:prose-base md:prose-lg
+        prose-h1:no-underline max-w-5xl mx-auto prose-zinc dark:prose-invert prose-img:rounded-lg"
+      >
+        <ContentRenderer v-if="articles" :value="articles">
+          <template #empty>
+            <p>No content found.</p>
+          </template>
+        </ContentRenderer>
+      </div>
+    </div>
+    <BlogToc />
+
+    <div class="flex flex-row gap-2">
+      <SocialShare
+        v-for="network in ['facebook', 'twitter', 'linkedin', 'email']"
+        :key="network"
+        :network="network"
+        :styled="true"
+        :label="false"
+        class="p-4"
+      />
+    </div>
+  </div>
+</template>
